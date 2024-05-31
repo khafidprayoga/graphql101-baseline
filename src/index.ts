6d10ba1950bfb21fc79311ca56e89c0961b6c542
Khafid Prayoga <khafidp@pm.me>
2024-05-31 10:01:24 +0700

    mocking schema

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { loadFiles } from "@graphql-tools/load-files";
+import { addMocksToSchema } from "@graphql-tools/mock";
+import { makeExecutableSchema } from "@graphql-tools/schema";
 
 async function bootstrap() {
   const schema = await loadFiles("src/**/*.graphql");
   const server = new ApolloServer({
-    typeDefs: schema,
+    schema: addMocksToSchema({
+      schema: makeExecutableSchema({
+        typeDefs: schema,
+      }),
+    }),
   });
 
   const { url } = await startStandaloneServer(server);
