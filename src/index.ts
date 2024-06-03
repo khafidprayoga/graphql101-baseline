24810a25618a97e11a4ce33209a3d28d040da648
Khafid Prayoga <khafidp@pm.me>
2024-06-03 21:44:16 +0700

    add customized mocked data entity (Book)

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,30 @@ import { loadFiles } from "@graphql-tools/load-files";
 import { addMocksToSchema } from "@graphql-tools/mock";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 
+const Book = {
+  id: +new Date(),
+  title: "Harry Potter and the Chamber of Secrets",
+  description:
+    "Harry Potter and the Chamber of Secrets is a fantasy novel written by British author J.K. Rowling and the second novel in the Harry Potter series.",
+  author: "J.K. Rowling",
+  totalPage: 251,
+  releaseYear: 1998,
+  isbn: "0439064864",
+};
+
 async function bootstrap() {
   const schema = await loadFiles("src/**/*.graphql");
+  const mocks = {
+    Int: () => Math.floor(Math.random() * 100),
+    Book: () => Book,
+  };
+
   const server = new ApolloServer({
     schema: addMocksToSchema({
       schema: makeExecutableSchema({
         typeDefs: schema,
       }),
+      mocks,
     }),
   });
 
