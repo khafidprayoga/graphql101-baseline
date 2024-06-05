52441fd239325637232b936e73554d0458c35132
Khafid Prayoga <khafidp@pm.me>
2024-06-05 16:28:26 +0700

    allow any, and add example resolver for query with 4 params signature on get detail book

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,25 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { loadFiles } from "@graphql-tools/load-files";
-import { addMocksToSchema } from "@graphql-tools/mock";
 import { makeExecutableSchema } from "@graphql-tools/schema";
+import { StoreDataSource } from "./store";
 
-const Book = {
-  id: +new Date(),
-  title: "Harry Potter and the Chamber of Secrets",
-  description:
-    "Harry Potter and the Chamber of Secrets is a fantasy novel written by British author J.K. Rowling and the second novel in the Harry Potter series.",
-  author: "J.K. Rowling",
-  totalPage: 251,
-  releaseYear: 1998,
-  isbn: "0439064864",
+const store = new StoreDataSource();
+const resolvers = {
+  Query: {
+    book(parent, { id }, ctx, info) {
+      return store.getBook(id);
+    },
+  },
 };
 
 async function bootstrap() {
   const schema = await loadFiles("src/**/*.graphql");
-  const mocks = {
-    Int: () => Math.floor(Math.random() * 100),
-    Book: () => Book,
-  };
 
   const server = new ApolloServer({
-    schema: addMocksToSchema({
-      schema: makeExecutableSchema({
-        typeDefs: schema,
-      }),
-      mocks,
+    schema: makeExecutableSchema({
+      typeDefs: schema,
+      resolvers,
     }),
   });
 
