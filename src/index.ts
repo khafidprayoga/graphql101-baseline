ab8089d3c1e44f02ca3837a7a2a045ff4ad8315a
Khafid Prayoga <khafidp@pm.me>
2024-06-07 20:44:15 +0700

    add get book related to author model
    add get books with pagination
    add author data source

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,30 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { loadFiles } from "@graphql-tools/load-files";
 import { makeExecutableSchema } from "@graphql-tools/schema";
-import { StoreDataSource } from "./store";
+import { StoreDataSource, AuthorDataSource, getBooksPagination } from "./store";
 
 const store = new StoreDataSource();
+const authorStore = new AuthorDataSource();
 const resolvers = {
   Query: {
     book(parent, { id }, ctx, info) {
-      return store.getBook(id);
+      const book = store.getBook(id);
+      return book;
+    },
+
+    books(parent, { page, size, authorId }, ctx, info) {
+      const req: getBooksPagination = {
+        page: page || 1,
+        size: size || 10,
+        authorId: authorId || null,
+      };
+      const bookList = store.getBooks(req);
+      return bookList;
+    },
+  },
+  Book: {
+    author(parent, args, ctx, info) {
+      return authorStore.getAuthor(parent.authorId);
     },
   },
 };
--- src/store.ts
+++ src/store.ts
@@ -1,25 +1,52 @@
 import * as fs from "fs";
 import * as path from "path";
 
-interface Book {
+export interface Book {
   id: string;
   title: string;
   description: string;
-  author: string;
+  authorId: string;
+  author: Author;
   totalPage: number;
   releaseYear: number;
   isbn: String;
 }
 
-type _getBooksPagination = {
+export interface Author {
+  id: string;
+  fullName: string;
+}
+
+export type getBooksPagination = {
   page: number;
   size: number;
+  authorId?: string;
 };
 
 type _getBooksResult = {
   books: Book[];
   count: number;
 };
+
+export class AuthorDataSource {
+  private authors: Author[] = [];
+  constructor() {
+    const location = path.join(__dirname, "authors.json");
+    const data = fs.readFileSync(location, "utf-8");
+    this.authors = JSON.parse(data);
+    console.log("seed authors data succeed");
+  }
+
+  getAuthor(authorId: string): Author | null {
+    const author =
+      this.authors.find((author) => author.id === authorId) || null;
+    if (author !== null) {
+      console.log("get author by id", authorId, author);
+    }
+
+    return author;
+  }
+}
 
 export class StoreDataSource {
   private books: Book[] = [];
@@ -48,10 +75,32 @@ export class StoreDataSource {
     return this.books.find((book) => book.id === bookId) || null;
   }
 
-  getBooks(paging: _getBooksPagination): _getBooksResult {
+  getBooks(paging: getBooksPagination): _getBooksResult {
     const { page, size } = paging;
     const start = (page - 1) * size;
     const end = start + size;
+
+    if (paging.authorId) {
+      switch (true) {
+        case paging.authorId === "unknown":
+          const noAuthorBooks = this.books
+            .filter((book) => book.authorId === "")
+            .slice(start, end);
+          return {
+            books: noAuthorBooks,
+            count: noAuthorBooks.length,
+          };
+
+        default:
+          const authorBookFiltered = this.books
+            .filter((book) => book.authorId === paging.authorId)
+            .slice(start, end);
+          return {
+            books: authorBookFiltered,
+            count: authorBookFiltered.length,
+          };
+      }
+    }
 
     return {
       books: this.books.slice(start, end),
